ebae7680700e620f123e7e2a9bb64e9f185dd45e
fengyehong123 <1355930128@qq.com>
2026-01-12 09:08:53 +0900

    反射

---
+++ ConsoleApp2/02-反射/01-反射.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace ConsoleApp2._02_反射
+{
+	// 🔷定义一个特性
+	[
+		AttributeUsage(
+			// 值允许作用在类上
+			AttributeTargets.Class,
+			// 不允许多次使用
+			AllowMultiple = false
+		)
+	]
+	public class AuthorizeAttribute(string role) : Attribute
+	{
+		public string Role { get; } = role;
+	}
+
+	[Authorize("Admin")]
+	public class User
+	{
+		public int Id { get; set; }
+		public string? Name { get; set; }
+		public int Age { get; set; }
+		// 声明 Address 并不是必须项目, 可以为 null
+		public string? Address { get; set; }
+
+		// 非静态方法
+		public void PrintUerInfo1(string msg1, string msg2)
+		{
+			Console.WriteLine($"{Name} {msg1}, {msg2}");
+		}
+
+		// 静态方法
+		private static void PrintUerInfo2(string msg1, string msg2)
+		{
+			Console.WriteLine($"你好{msg1}, {msg2}");
+		}
+	}
+
+	/*
+		🔷反射 = 在运行时检查和操作程序集、类型、成员的能力
+			简单说就是：
+			程序在“运行中”，还能去看自己是什么类、有什么方法、属性、特性，然后调用它们
+	 */
+	public class _01_反射
+	{
+		// 创建一个类对象
+		private static readonly User user1 = new()
+		{
+			Id = 110120,
+			Name = "Tom",
+			Age = 25,
+			Address = "宇宙"
+		};
+
+		// 🔷Type 是整个 C# 的反射的核心入口
+		public static void PrintInfo1()
+		{
+
+			// 获取Type的方式1：编译期确定, 最常用
+			Type t1 = typeof(User);
+			Console.WriteLine(t1);
+
+			// 获取Type的方式2：运行期获取
+			Type t2 = user1.GetType();
+			Console.WriteLine(t2);
+
+			// 获取Type的方式3：通过字符串来获取, 完全动态
+			Type? t3 = Type.GetType("ConsoleApp2._02_反射.User");
+			Console.WriteLine(t3);
+		}
+
+		public static void PrintInfo2() 
+		{
+			// 🔷查看类的基本信息
+			Type userType = typeof(User);
+
+			// 类名称
+			Console.WriteLine(userType.Name);  // User
+			// 全名称
+			Console.WriteLine(userType.FullName);  // ConsoleApp2._02_反射.User
+			// 命名空间
+			Console.WriteLine(userType.Namespace);  // ConsoleApp2._02_反射
+			// 是否是类
+			Console.WriteLine(userType.IsClass);  // True
+
+			// 🔷获取属性
+			PropertyInfo[] properties = userType.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+			foreach (PropertyInfo property in properties)
+			{
+				Console.WriteLine(property.Name);
+				// Id
+				// Name
+				// Age
+				// Address
+			}
+
+			// 🔷获取方法名称
+			MethodInfo[] methods = userType.GetMethods();
+			foreach (MethodInfo methodInfo in methods)
+			{
+				Console.WriteLine(methodInfo.Name);
+				// get_Id
+				// set_Id
+				// get_Name
+				// set_Name
+				// get_Age
+				// set_Age
+				// get_Address
+				// set_Address
+				// PrintUerInfo
+				// GetType
+				// ToString
+				// Equals
+				// GetHashCode
+			}
+
+			// 调用普通方法
+			Type typeUser = user1.GetType();
+
+			// 因为 MethodInfo 可能为null ,所以使用 ? 来修饰
+			MethodInfo? method1 = typeUser.GetMethod("PrintUerInfo1");
+			// 避免为null的	method使用Invoke方法报错, 所以使用 method?.Invoke
+			method1?.Invoke(user1, ["admin", "123456"]);  // Tom admin, 123456
+
+			// 因为 PrintUerInfo2 是私有方法, 因此在反射的时候必须指定 BindingFlags.NonPublic
+			// 否则会按照默认的共有方法规则去寻找, 然后会找不到方法
+			MethodInfo? method2 = typeUser.GetMethod("PrintUerInfo2", BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic);
+			method2?.Invoke(user1, ["admin", "123456"]);  // 你好admin, 123456
+		}
+
+		public static void PrintInfo3() 
+		{
+			// 创建Type对象
+			Type userType = typeof(User);
+			// 通过反射创建对象
+			object? obj = Activator.CreateInstance(userType);
+			if (obj != null) 
+			{
+				// 通过反射动态的设置类的属性值
+				PropertyInfo? propId = userType.GetProperty("Id");
+				propId?.SetValue(obj, 110120);
+
+				PropertyInfo? propName = userType.GetProperty("Name");
+				propName?.SetValue(obj, "贾飞天");
+
+				PropertyInfo? propAge = userType.GetProperty("Age");
+				propAge?.SetValue(obj, 18);
+
+				// 转换为User对象
+				User user = (User)obj;
+				Console.WriteLine(user);
+			}
+
+			// 获取类上的特性
+			AuthorizeAttribute? authorizeAttribute = userType.GetCustomAttribute<AuthorizeAttribute>();
+			if (authorizeAttribute != null)
+			{
+				// 获取类特性上设定的值
+				Console.WriteLine(authorizeAttribute.Role);  // Admin
+			}
+		}
+	}
+}
---
+++ ConsoleApp2/02-反射/02-反射.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace ConsoleApp2._02_反射
+{
+
+	public class Student
+	{
+		public int Id { get; set; }
+		// 声明 Name 并不是必须项目, 可以为 null
+		public string? Name { get; set; }
+		public int Age { get; set; }
+		// 声明 Address 并不是必须项目, 可以为 null
+		public string? Address { get; set; }
+	}
+
+	public class _02_反射封装 
+	{
+		public static void PrintInfo()
+		{
+			// 定义一个字典
+			Dictionary<string, object> paramDict = new()
+			{
+				["Id"] = 10086,
+				["Name"] = "李四",
+				["Age"] = 20
+			};
+
+			Type studentType = typeof(Student);
+			Student student1 = (Student)MapToObject1(studentType, paramDict);
+
+			// 打印类对象中的信息
+			Console.WriteLine(student1.Id);
+			Console.WriteLine(student1.Name);
+			Console.WriteLine(student1.Age);
+			Console.WriteLine("________________________________________________");
+
+			// 使用更加进阶版的封装方法
+			Student student2 = MapToObject2<Student>(paramDict);
+
+			// 打印类对象中的信息
+			Console.WriteLine(student2.Id);
+			Console.WriteLine(student2.Name);
+			Console.WriteLine(student2.Age);
+		}
+
+		// 封装一个通过反射进行赋值的方法
+		public static object MapToObject1(Type type, Dictionary<string, object> data)
+		{
+			// ! 的作用是强行指定创建后的obj对象不可能为null, 防止编译器的检测
+			object obj = Activator.CreateInstance(type)!;
+
+			foreach (var kv in data)
+			{
+				PropertyInfo prop = type.GetProperty(kv.Key)!;
+				if (prop == null) continue;
+				if (!prop.CanWrite) continue;
+
+				// 类型转换（非常关键）
+				object value = Convert.ChangeType(kv.Value, prop.PropertyType);
+				prop.SetValue(obj, value);
+			}
+
+			return obj;
+		}
+
+		// 更加进阶版的封装
+		public static T MapToObject2<T>(Dictionary<string, object> data) where T : new()
+		{
+			var obj = new T();
+			var type = typeof(T);
+
+			foreach (var kv in data)
+			{
+				var prop = type.GetProperty(kv.Key);
+				if (prop == null || !prop.CanWrite)
+				{
+					continue;
+				}
+
+				object value = kv.Value;
+				if (value == null)
+				{
+					continue;
+				}
+
+				Type targetType = Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType;
+
+				if (targetType.IsEnum)
+				{
+					// 若 value 为 null 的话, 早就 continue 了, 因此此处不可能为null, 因此使用 ! 强行指定
+					value = Enum.Parse(targetType, value.ToString()!);
+				}
+				else
+				{
+					value = Convert.ChangeType(value, targetType);
+				}
+
+				prop.SetValue(obj, value);
+			}
+
+			return obj;
+		}
+	}
+}
