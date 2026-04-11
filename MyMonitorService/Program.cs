f848fd5bdd978cb8498e4d5e2400a18f25b039d7
fengyehong123 <1355930128@qq.com>
2026-04-11 21:05:14 +0900

    MyMonitorService

---
+++ MyMonitorService/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+using MyMonitorService;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+Mutex? mutex = null;
+
+try
+{
+    mutex = new Mutex(true, "MyMonitorService", out bool createdNew);
+    if (!createdNew)
+    {
+        return;
+    }
+
+	// 获取当前Host的Builder对象
+	IHostBuilder builder = Host.CreateDefaultBuilder(args);
+
+	// 为Builder对象添加服务
+	builder.ConfigureServices((context, services) =>
+	{
+		// 添加服务所需的配置文件类
+		services.Configure<ProcessMonitorOptions>(
+			context.Configuration.GetSection("ProcessMonitor")
+		);
+
+		// 添加服务所需的Worker
+		services.AddHostedService<Worker>();
+	});
+
+	IHost host = builder.Build();
+	host.Run();
+}
+finally
+{
+    mutex?.ReleaseMutex();
+    mutex?.Dispose();
+}
---
+++ MyMonitorService/Worker.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+// 更加简洁的定义namespace的方法
+namespace MyMonitorService;
+
+/*
+	C#12的 主构造函数（Primary Constructor），属于一个比较新的语法
+	类定义时直接声明构造函数参数
+	
+	传统写法
+	public class Worker : BackgroundService
+	{
+		private readonly ILogger<Worker> _logger;
+		private readonly IOptionsMonitor<ProcessMonitorOptions> _options;
+
+		public Worker(
+			ILogger<Worker> logger,
+			IOptionsMonitor<ProcessMonitorOptions> options)
+		{
+			_logger = logger;
+			_options = options.CurrentValue;
+		}
+	}
+	
+	当使用 services.AddHostedService<Worker>(); 的时候
+	Worker会被注册到DI容器, .NET在创建Worker时
+		1. 会分析构造函数
+			当发现需要ILogger<Worker> 和 IOptionsMonitor<ProcessMonitorOptions> 的时候
+		2. 去容器里找services 里找
+			CreateDefaultBuilder() 自动注册了 ILogger<Worker>
+			Configure<>() 手动注册了 IOptionsMonitor<ProcessMonitorOptions>
+		3. 然后会帮我们自动注入, 相当于替我们做了以下工作
+			new Worker(logger实例, options实例)
+	
+	总结
+		DI容器会根据构造函数自动解析依赖
+*/
+public class Worker(ILogger<Worker> logger, IOptionsMonitor<ProcessMonitorOptions> optionsMonitor) : BackgroundService
+{
+	// 日志对象
+	private readonly ILogger<Worker> _logger = logger;
+	// 配置文件对象
+	private readonly IOptionsMonitor<ProcessMonitorOptions> _optionsMonitor = optionsMonitor;
+
+	protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+	{
+		while (!stoppingToken.IsCancellationRequested)
+		{
+			try
+			{
+				// 每次循环都获取最新的配置
+				var options = _optionsMonitor.CurrentValue;
+
+				// 读取配置文件中的线程名
+				foreach (var processName in options.ProcessNames)
+				{
+					// 获取线程名对象
+					var processes = Process.GetProcessesByName(processName);
+
+					foreach (var proc in processes)
+					{
+						// 如果当前线程的运行时间 < 指定的最大时间的话
+						var runTime = DateTime.Now - proc.StartTime;
+						if (runTime.TotalMinutes < options.MaxMinutes)
+						{
+							// 跳过, 不杀死线程
+							continue;
+						}
+
+						// 杀死指定的线程
+						try
+						{
+							_logger.LogWarning("Killing {Process} PID={Pid}", processName, proc.Id);
+							proc.Kill();
+						}
+						catch (Exception ex)
+						{
+							_logger.LogError(ex, "Kill error: {Process}", processName);
+						}
+					}
+				}
+
+				// 延时
+				await Task.Delay(options.CheckIntervalSeconds * 1000, stoppingToken);
+			}
+			catch (TaskCanceledException)
+			{
+				return;
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex, "Main loop error");
+			}
+		}
+	}
+}
